da736c3720abb55d1eb9226709c59c9199d1a668
Novan Andriyono <evelynradtya@gmail.com>
2018-04-19 20:39:22 +0700

    change all this class

--- src/Dpscan.php
+++ src/Dpscan.php
@@ -1,106 +1,127 @@
 <?php
 namespace Dpscan;
+
 use Dpscan\Contracts\Dpscan as DpscanInterface;
 use Illuminate\Database\Eloquent\Collection;
 use Exception;
 
 class Dpscan implements DpscanInterface
 {
-	protected $rootfolder,$results;
 
-	public function __construct($rootfolder = null,$results = null){
-		$this->rootfolder = $rootfolder;
-		$this->results = $results;
+	protected $rootfolder;
+	/**
+     * The items contained in the collection.
+     *
+     * @var array
+     */
+    protected $items = [];
+
+	public function __construct(array $items = []){
+		if(func_num_args() !== 1){
+			return;
+		}
+		$this->items = $items;
 	}
 
 	public function setdir($dir = null){
-		if((is_dir($dir) === true) || (str_contains($dir,base_path()) === true)){
-			$this->rootfolder = $dir;
-			return $this;
+		if(func_num_args() !== 1){
+			return;
 		}
-		throw new Exception("Not Dir", 1);
+		if(str_contains($dir,$this->config()->root) !== true){
+			return;
+		}
+		if((is_dir($dir) !== true)
+			|| (str_contains($dir,base_path()) !== true)){
+			return;
+		}
+		$this->rootfolder = $dir;
+		return $this;
 	}
 
 	public function get(){
-		$this->rootfolder = $this->rootfolder;
-		$this->results = $this->getContent($this->rootfolder);
-		return $this;
+		return $this->getContent();
 	}
 
-	public function all(){
-		return $this->setAll();
+	public function onlydir(){
+		return $this->setonlydir();
 	}
 
-	protected function setAll(){
-		$this->rootfolder = $this->rootfolder;
-		$this->results = $this->getAllContent($this->get()->results,$this->rootfolder);
-		return $this;
+	protected function setonlydir(){
+		$lists = $this->get()->items()->toArray();
+		$class = get_class();
+		$onlydir = new static(array_keys(array_flip($lists)));
+		return $onlydir->setdir($this->rootfolder);
 	}
 
-	public function results(){
-		return $this->setResults();
+	public function onlyfiles(){
+		return $this->setonlyfiles();
 	}
 
-	protected function setResults(){
-		$results = $this->results;
-		unset($results['file']);
-		unset($results['dir']);
-		return $this->items(array_values($results));
-	}
-
-	public function getfiles(){
-		return $this->setGetFiles();
-	}
-
-	protected function setGetFiles(){
-		$results = $this->results['file'];
-		return $this->items(array_values($results));
-	}
-
-	public function getdir(){
-		return $this->setGetDir();
-	}
-
-	protected function setGetDir(){
-		$results = $this->results['dir'];
-		return $this->items(array_values($results));
-	}
-
-	protected function getAllContent($list = [], $root = null, $now = 0, $results = []){
-		if(is_object($list)){
-			$total = $list->count();
-		}elseif(is_string($list)){
-			$total = 0;
-		}else{
-			$total = count($list);
-		}
-		if($now !== $total){
-			$item = $root.DIRECTORY_SEPARATOR.$list[$now];
-			//unset($this->results[$now]);
-			$results[$item] = $item;
-			if(is_file($item)){
-				$results['file'][] = $item;
+	protected function setonlyfiles(){
+		$lists = array_keys($this->get()->items()->toArray());
+		$onlydir = array_flip($this->onlydir()->items()->toArray());
+		for ($i=0; $i < count($lists) ; $i++) {
+				$item = $lists[$i];
+			if(isset($onlydir[$item]) === true){
+				unset($lists[$i]);
 			}
+		}
+		$results = new static(array_values($lists));
+		return $results->setdir($this->rootfolder);
+	}
+
+	protected function getContent(){
+		return $this->setContent();
+	}
+
+	protected function setContent(){
+		if(func_num_args() !== 0){
+			return;
+		}
+		if(str_contains($this->rootfolder,$this->config()->root) !== true){
+			return;
+		}
+		if((is_dir($this->rootfolder) === true) &&
+			(is_writable($this->rootfolder) === true)){
+			$this->items = $this->fixArray(scandir($this->rootfolder));
+			$this->items = new static($this->items);
+			return $this->items->setdir($this->rootfolder)->getAllContent();
+		}
+		return $this->items;
+	}
+
+	public function items(){
+		return $this->getitems();
+	}
+
+	protected function getitems(){
+		$this->items = new static($this->items);
+		return $this->items->setitems();
+	}
+
+	protected function setitems(){
+		return new Collection($this->items);
+	}
+
+	protected function getAllContent($now = 0, $results = []){
+		if(str_contains($this->rootfolder,$this->config()->root) !== true){
+			return;
+		}
+		$total = count($this->items);
+		if($now !== $total){
+			$item = $this->rootfolder.DIRECTORY_SEPARATOR.$this->items[$now];
+			$results[$item] = $this->rootfolder;
 			if(is_dir($item)){
-				$child = (new Dpscan)->setdir($item)->all();
-				$results['dir'][] = $child->rootfolder;
-				$results[$item] = $child->rootfolder;;
-				$results = $results + $child->results;
+				$newfolder = new static([]);
+				$newitems = $newfolder->setdir($item)->get();
+				$results = $results + $newitems->items;
 			}
 			$next = $now + 1;
-			return $this->getAllContent($list,$root, $next,$results);
+			return $this->getAllContent($next,$results);
 		}
+		$results = new static($results);
+		$results = $results->setdir($this->rootfolder);
 		return $results;
-	}
-
-	protected function items(array $array = []){
-		return new Collection($array);
-	}
-
-	protected function getContent($dir=null){
-		if(is_dir($dir)){
-			return $this->fixArray(scandir($dir));
-		}
 	}
 
 	protected function fixArray($array = []){
@@ -109,4 +130,7 @@ class Dpscan implements DpscanInterface
         return array_values($array);
 	}
 
+	private function config(){
+        return (object) config('dpscan');
+    }
 }
