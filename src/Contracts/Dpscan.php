3de4ebd99dc607c5ad5eeb4e919e29991d4f6c83
Novan Andriyono <evelynradtya@gmail.com>
2018-04-24 06:42:48 +0700

    add new interface

--- src/Contracts/Dpscan.php
+++ src/Contracts/Dpscan.php
@@ -2,9 +2,23 @@
 namespace Dpscan\Contracts;
 
 interface Dpscan{
-	public function setdir($dir);
+	public function setdir(string $dir);
 	public function get();
 	public function onlyfiles();
 	public function onlydir();
+	public function notcontains(array $array = []);
+	public function notcontainsfiles(array $array = []);
+	public function notcontainsdir(array $array = []);
+	public function contains(array $array = []);
+	public function containsfiles(array $array = []);
+	public function containsdir(array $array = []);
+	public function except(array $array);
+	public function regexcept(array $array);
+	public function regexceptfiles(array $array);
+	public function regexceptdir(array $array);
+	public function regexonly(array $array);
+	public function regexonlyfiles(array $array);
+	public function regexonlydir(array $array);
 	public function items();
+	public function rootchange(string $dir);
 }
